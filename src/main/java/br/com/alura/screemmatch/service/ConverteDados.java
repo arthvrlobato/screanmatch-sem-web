213043116a328bd9c5aebb3a54184f91c4089bc5
Arthur <arthurlobatoc@gmail.com>
2025-05-20 15:50:00 -0300

    Novas funcionalidades no projeto

--- src/main/java/br/com/alura/screemmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screemmatch/service/ConverteDados.java
@@ -4,7 +4,7 @@ import br.com.alura.screemmatch.model.DadosSerie;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConverteDados implements IConverteDados{
+public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
 
 
