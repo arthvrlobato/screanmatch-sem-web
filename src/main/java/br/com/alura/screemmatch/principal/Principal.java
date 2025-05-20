d2660426658ce59bbee83379c99eeb726d72ad5c
Arthur <arthurlobatoc@gmail.com>
2025-05-20 19:32:14 -0300

    Erro ao imprimir o nome dos eps, corrigir amanha

--- src/main/java/br/com/alura/screemmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screemmatch/principal/Principal.java
@@ -7,8 +7,10 @@ import br.com.alura.screemmatch.service.ConsumoApi;
 import br.com.alura.screemmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -35,11 +37,26 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
-        for (int i = 0; i < dados.totalTemporadas(); i++){
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for (int j = 0; j < episodiosTemporada.size(); j++){
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        }
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+                //.toList();
+
+        System.out.println("Top 5 eps: ");
+        dadosEpisodios.stream()
+                .filter(e -> !e.titulo().equalsIgnoreCase("N/A"))
+                .filter(e-> e.avaliacao() !=null)
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(3)
+                .forEach(System.out::println);
+
     }
 }
